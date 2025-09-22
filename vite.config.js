6afd8dc61c65aed5f3eaf8a18ebc899775c76216
ojaswiniVandrangi <ojaswini.vandrangi@gmail.com>
2025-09-23 02:30:05 +0530

    OK

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Web_Development_LearningApp/', // Replace with your repository name
   plugins: [react()],
-  base: './', // ensures production build works from dist folder
 });
